128bcf728e48386a60ae00486d88540c7074359a
Makey <leobee1994@gmail.com>
2020-02-10 19:14:25 +1100

    Update pgunion.sh

--- pgunion.sh
+++ pgunion.sh
@@ -9,4 +9,4 @@ sleep 2
 
 hdpath="$(cat /home15/maki/plexguide/server.hd.path)"
 mergerfs -o sync_read,auto_cache,dropcacheonclose=true,use_ino,allow_other,func.getattr=newest,category.create=ff,minfreespace=0,fsname=pgunion \
-$hdpath/move=RW:$hdpath/downloads=RW:/home15/maki/mnt/tdrive=NC:/mnt/gdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC /home15/maki/mnt/unionfs
+$hdpath/move=RW:$hdpath/downloads=RW:/home15/maki/mnt/tdrive=NC:/home15/maki/mnt/gdrive=NC:/home15/maki/mnt/tcrypt=NC:/home15/maki/mnt/gcrypt=NC /home15/maki/mnt/unionfs
